af93000c9f7ac263b049aea54d4063d31c78f059
vaishnavBorade <115063068+vaishnavBorade@users.noreply.github.com>
2024-06-22 10:31:20 +0530

    d

--- 08 RestAPI/index.js
+++ 08 RestAPI/index.js
@@ -1,7 +1,35 @@
 const express = require("express");
+const users = require("./MOCK_DATA.json");
 
 const app = express();
 const PORT = 8000;
 
-app.listen(PORT, () => console.log(`Server Started at Port: ${PORT}`)
-)
+app.get("/", (req, res) => {
+  return res.send("Welcome to the User API. Go to /users to see the user data.");
+});
+
+//sending data by creating html which can be only used by a browser. it is rendered in server side.
+app.get("/api/users",(req,res)=>{
+  return res.json(users);
+})
+
+//sending data in the form of JSON.
+app.get("/users",(req,res)=>{
+  const html = `
+  <ul>
+    ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+  </ul>
+  `;
+  res.send(html);
+})
+
+//getting data of a specific user of ID "id"
+app.get("/api/users/:id",(req,res)=>{
+  const id = Number(req.params.id);
+  const user = users.find((user)=>user.id===id);
+  return res.json(user);
+}) 
+
+
+
+app.listen(PORT, () => console.log(`Server Started at Port: ${PORT}`))
