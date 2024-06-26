46fbeee1c12d9805e6edcb914525f4f933c9e739
vaishnavBorade <115063068+vaishnavBorade@users.noreply.github.com>
2024-06-26 12:15:49 +0530

    df

---
+++ 10 MongoDB Connection/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const mongoURL = '';
+
+mongoose.connect(mongoURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+const db = mongoose.connection;
+
+db.on('connected', () => console.log("Database Connection Successful."));
+
+db.on('error', () => console.log("Database Connection Error."));
+
+db.on('disconnected', () => console.log("Database Dsiconnected."));
+
+module.exports = db;
--- 10 MongoDB Connection/index.js
+++ 10 MongoDB Connection/index.js
@@ -1,3 +1,16 @@
 //npm init
 //npm i mongoose
 //npm i express
+//npm i nodemon
+const db = require("./db");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my Hotel. This is /users GET api");
+})
+
+app.listen(8000, ()=>{
+  console.log("Server is running")
+})
